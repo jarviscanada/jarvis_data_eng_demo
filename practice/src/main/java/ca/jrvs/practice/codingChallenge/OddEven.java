81b82e2fd83e5987cb5493180f656f289cc8eb75
Edward Wang <edward@jrvs.ca>
2019-12-07 20:03:53 -0500

    update sample code solution

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/OddEven.java
@@ -0,0 +1,24 @@
+package ca.jrvs.practice.codingChallenge;
+
+/**
+ * ticket: https://www.notion.so/jarviscanada/Check-if-a-number-is-even-or-odd-4cbdd9acd9504c04b9b4ef0213f8084d
+ */
+public class OddEven {
+
+  /**
+   * Big-O: O(1)
+   * Justification: it's an arithmetic operation
+   */
+  public String oddEvenMod(int i){
+    return i % 2 == 0 ? "even" : "odd";
+  }
+
+  /**
+   * Big-O:
+   * Justification:
+   */
+  public String oddEvenBit(int i){
+    return null;
+  }
+
+}
